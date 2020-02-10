e2f5252deb4db62897be8d0d972c95b8a39f722e
naoyuki goko <bloody_schedule0548@yahoo.co.jp>
2020-02-10 09:59:18 +0000

    基本的なユーザーモデル作成とホームやアカウント作成のレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
