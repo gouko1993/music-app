39fa478f81a062cbf923e531a99b98af511d9b90
naoyuki goko <bloody_schedule0548@yahoo.co.jp>
2020-03-31 11:28:42 +0000

    ログイン機能の作成ユーザー一覧ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,57 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+   before_action :logged_in_user, only: [:index, :destroy]
+   before_action :admin_user,     only: :destroy
+  
+  
+    def index
+      @users = User.paginate(page: params[:page])
+    end
+    
+    def show
+      @user = User.find(params[:id])
+    end
+    
+    def new
+      @user = User.new
+    end
+    
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        flash[:success] = "登録が完了しました"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+    
+    def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "ユーザーを削除しました"
+      redirect_to users_url
+    end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  
 end
